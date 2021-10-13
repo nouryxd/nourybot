66eeeee7dcfa452570fd857a2f17a77e71cf06cb
lyx0 <stefan@parfuss.me>
2021-10-13 21:39:34 +0200

    Connect Disconnect

--- bot/bot.go
+++ bot/bot.go
@@ -12,15 +12,33 @@ type Bot struct {
 	log          *log.Logger
 }
 
-func (b *Bot) newTwitchClient() *twitch.Client {
-	twitchClient := twitch.NewClient(b.cfg.Username, b.cfg.Oauth)
-	return twitchClient
-}
-
 func NewBot(cfg *cfg.Config, log *log.Logger, twitchClient *twitch.Client) *Bot {
 	return &Bot{
 		cfg:          cfg,
 		twitchClient: twitchClient,
 		log:          log,
 	}
+}
+
+func (b *Bot) newTwitchClient() *twitch.Client {
+	twitchClient := twitch.NewClient(b.cfg.Username, b.cfg.Oauth)
+	return twitchClient
+}
+
+func (b *Bot) Connect() error {
+	err := b.twitchClient.Connect()
+	if err != nil {
+		log.Error("Error Connecting from Twitch: ", err)
+	}
+
+	return err
+}
+
+func (b *Bot) Disconnect() error {
+	err := b.twitchClient.Disconnect()
+	if err != nil {
+		log.Error("Error Disconnecting from Twitch: ", err)
+	}
+
+	return err
 }
