f5aca689c0f70a9db0bfd38c110b5bd248f92f40
lyx0 <stefan@parfuss.me>
2021-10-17 17:40:53 +0200

    reformat to make it more universal

--- pkg/api/aiden/aiden.go
+++ pkg/api/aiden/aiden.go
@@ -8,7 +8,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func Weather(uri string) string {
+func ApiCall(uri string) string {
 	resp, err := http.Get(fmt.Sprintf("https://customapi.aidenwallis.co.uk/%s", uri))
 	if err != nil {
 		log.Error(err)
