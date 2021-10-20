3de45edff09d8d8ca10560599ad3b63093298059
lyx0 <stefan@parfuss.me>
2021-10-21 00:40:20 +0200

    format

--- pkg/commands/uptime.go
+++ pkg/commands/uptime.go
@@ -10,11 +10,11 @@ import (
 
 func Uptime(channel string, name string, nb *bot.Bot) {
 	resp, err := aiden.ApiCall(fmt.Sprintf("api/v1/twitch/channel/%s/uptime", name))
+
 	if err != nil {
 		nb.Send(channel, "Something went wrong FeelsBadMan")
 		log.Error(err)
 	}
 
 	nb.Send(channel, resp)
-
 }
