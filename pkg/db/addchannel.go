d2a67305ea827ff65c9577778382b28fd4e3b915
lyx0 <stefan@parfuss.me>
2021-10-28 17:53:58 +0200

    little bit of cleanings

--- pkg/db/addchannel.go
+++ pkg/db/addchannel.go
@@ -10,27 +10,21 @@ import (
 )
 
 func AddChannel(target, channelName string, nb *bot.Bot) {
-	// Interact with data
-
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	/*
-		Get my collection instance
-	*/
 	collection := nb.MongoClient.Database("nourybot").Collection("channels")
 
 	// Channel
 	// {{"name": string}, {"connect": bool}}
 	chnl := Channel{channelName, true}
 
-	res, insertErr := collection.InsertOne(ctx, chnl)
+	_, insertErr := collection.InsertOne(ctx, chnl)
 	if insertErr != nil {
 		nb.Send(target, fmt.Sprintf("Error trying to join %s", channelName))
 		log.Error(insertErr)
 		return
 	}
-	nb.Send(target, fmt.Sprintf("Joined %s", channelName))
-	log.Info(res)
 
+	nb.Send(target, fmt.Sprintf("Joined %s", channelName))
 }
