36d36b494267eabb1fd27fc5b617e11a19e60f76
lyx0 <stefan@parfuss.me>
2021-10-28 22:28:26 +0200

    add tests for aidens api

--- pkg/api/aiden/aiden_test.go
+++ pkg/api/aiden/aiden_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 )
 
+// TestAidenBotRatelimits calls aidens bot ratelimits api and checks
+// the results against each other with my bot and another verified bot.
 func TestAidenBotRatelimits(t *testing.T) {
 	t.Run("returns a twitch accounts ratelimits", func(t *testing.T) {
 		requestPajbot, _ := ApiCall("api/v1/twitch/botStatus/pajbot?includeLimits=1")
