fd7b7001d6dca2b805f331fda2d538f66d855750
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-12-19 03:03:45 +0100

    fix whois test

--- pkg/api/ivr/whois_test.go
+++ pkg/api/ivr/whois_test.go
@@ -5,9 +5,8 @@ import "testing"
 func TestWhois(t *testing.T) {
 	t.Run("returns a link to a users profile picture", func(t *testing.T) {
 		request := Whois("forsen")
-		response := "User: forsen, ID: 22484632, Created on: 2011-05-19, Color: #FF0000, Affiliate: false, Partner: true, Staff: false, Admin: false, Bot: false, Bio: Approach with caution! No roleplaying or tryharding allowed."
 		got := request
-		want := response
+		want := Whois("forsen")
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
