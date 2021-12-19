c9d06b8a0485e11458d8bb29f0e33517490c0dba
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-12-19 02:54:04 +0100

    fix whois test

--- pkg/api/ivr/whois_test.go
+++ pkg/api/ivr/whois_test.go
@@ -6,7 +6,6 @@ func TestWhois(t *testing.T) {
 	t.Run("returns a link to a users profile picture", func(t *testing.T) {
 		request := Whois("forsen")
 		response := "User: forsen, ID: 22484632, Created on: 2011-05-19, Color: #FF0000, Affiliate: false, Partner: true, Staff: false, Admin: false, Bot: false, Bio: Approach with caution! No roleplaying or tryharding allowed."
-
 		got := request
 		want := response
 
