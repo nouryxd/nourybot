d4d15a82e32533b1518ef5d0231468d326d479f6
lyx0 <66651385+lyx0@users.noreply.github.com>
2022-06-21 00:08:37 +0200

    asd

--- cmd/bot/whispermessage.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/gempir/go-twitch-irc/v3"
-
-func (app *application) handleWhisperMessage(message twitch.WhisperMessage) {
-	// Print the whisper message for now.
-	// TODO: Implement a basic whisper handler.
-	app.logger.Infof("[#whisper]:%s: %s", message.User.DisplayName, message.Message)
-}
