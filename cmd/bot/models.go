67fb901065a88dad48110fd3a7e862ece35d1a16
lyx0 <66651385+lyx0@users.noreply.github.com>
2022-08-10 22:06:46 +0200

    move config and Application struct to models

--- cmd/bot/models.go
+++ cmd/bot/models.go
@@ -1,8 +1,34 @@
 package main
 
-import "errors"
+import (
+	"database/sql"
+	"errors"
+
+	"github.com/gempir/go-twitch-irc/v3"
+	"github.com/lyx0/nourybot/internal/data"
+	"go.uber.org/zap"
+)
 
 var (
 	ErrUserLevelNotInteger = errors.New("user level must be a number")
 	ErrRecordNotFound      = errors.New("user not found in the database")
 )
+
+type config struct {
+	twitchUsername string
+	twitchOauth    string
+	environment    string
+	db             struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
+}
+
+type Application struct {
+	TwitchClient *twitch.Client
+	Logger       *zap.SugaredLogger
+	Db           *sql.DB
+	Models       data.Models
+}
