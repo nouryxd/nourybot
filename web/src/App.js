69819e4241e83ed269eda87290adf96af5015f14
lyx0 <66651385+lyx0@users.noreply.github.com>
2022-08-18 22:01:53 +0200

    add web

---
+++ web/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Command from './Command/Command'
+
+function App() {
+  return (
+    <div className="App">
+    <div className="wrapper">
+      <h1>Nourybot</h1>
+      <Command />
+    </div>
+    </div>
+  );
+}
+
+export default App;
