05c22e0093a269f9abdd3239fa53a9ec0c176578
lyx0 <66651385+lyx0@users.noreply.github.com>
2023-03-04 19:18:15 +0000

    add migration for lastfm_users

---
+++ migrations/000005_create_lastfm_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS lastfm_users (
+	id bigserial PRIMARY KEY,
+	twitch_login text UNIQUE NOT NULL,
+	twitch_id text NOT NULL,
+	lastfm_username text NOT NULL 
+);
+
+INSERT INTO lastfm_users (twitch_login,twitch_id,lastfm_username) VALUES
+	 ('nourylul','31437432','nouryqt');
+
