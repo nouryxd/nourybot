3d488ee373f3d2674913f7a0e756d3d08b9bd723
lyx0 <git@noury.ee>
2023-09-08 03:55:00 +0200

    add migrations

---
+++ migrations/000002_create_users_table.up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS users (
+	id bigserial PRIMARY KEY,
+	added_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+	login text UNIQUE NOT NULL,
+	twitchid text NOT NULL,
+	level integer,
+	location text,
+	lastfm_username text
+);
+
+INSERT INTO users (added_at,login,twitchid,"level",location,lastfm_username) VALUES
+	 (NOW(),'nourylul','31437432',1000,'vilnius','nouryqt'),
+	 (NOW(),'nourybot','596581605',1000,'',''),
+	 (NOW(),'uudelleenkytkeytynyt','465178364',1000,'',''),
+	 (NOW(),'xnoury','197780373',500,'',''),
+	 (NOW(),'noemience','135447564',500,'','');
---
+++ migrations/000004_create_timers_table.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS timers (
+    id bigserial PRIMARY KEY,
+    name text NOT NULL,
+    text text NOT NULL,
+    channel text NOT NULL,
+    repeat text NOT NULL
+);
+
+INSERT INTO timers (name,"text",channel,repeat) VALUES
+	 ('nourylul-60m','timer every 60 minutes :)','nourylul','60m'),
+	 ('nourylul-2m','timer every 2 minutes :)','nourylul','2m'),
+	 ('nourybot-60m','timer every 60 minutes :)','nourybot','60m'),
+	 ('nourybot-1h','timer every 1 hour :)','nourybot','1h'),
+	 ('xnoury-60m','timer every 420 minutes :)','xnoury','420m'),
+	 ('xnoury-1h','timer every 1 hour :)','xnoury','1h'),
+	 ('xnoury-15m','180 minutes timer :)','xnoury','180m');
+
