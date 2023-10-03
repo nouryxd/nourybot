5ae08fad7c6d5a81dc885e5b1c888d21153a749e
lyx0 <git@noury.cc>
2023-10-03 17:02:11 +0200

    feat: ()dl command: download given video with yt-dlp, then upload it to imagehost and post the link in response

--- cmd/nourybot/download.go
+++ cmd/nourybot/download.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"mime/multipart"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/wader/goutubedl"
@@ -18,13 +21,12 @@ func (app *application) Download(target, link string) {
 	if err != nil {
 		app.Log.Errorln(err)
 	}
-	rExt := result.Info.Ext
+	rExt := "mp4"
 	downloadResult, err := result.Download(context.Background(), "best")
 	if err != nil {
 		app.Log.Errorln(err)
 	}
 	app.Send(target, "Downloaded..")
-	defer downloadResult.Close()
 	fn, err := uuid.NewUUID()
 	if err != nil {
 		app.Log.Errorln(err)
@@ -40,10 +42,80 @@ func (app *application) Download(target, link string) {
 		app.Log.Errorln(err)
 	}
 
-	app.UploadCatbox(target, fmt.Sprintf("%s.%s", fn, rExt))
+	downloadResult.Close()
+	f.Close()
+	duration := 5 * time.Second
+	app.Send(target, "ResidentSleeper ..")
+	time.Sleep(duration)
 
-	//os.Remove(fmt.Sprintf("%s.mp4", fn))
+	app.upload(target, fmt.Sprintf("%s.%s", fn, rExt))
 
-	//app.TwitchClient.Say(channel, b.String())
+}
 
+func (app *application) upload(target, path string) {
+	const URL = "https://i.yaf.ee/upload"
+	app.Send(target, "Uploading .. dankCircle")
+	pr, pw := io.Pipe()
+	form := multipart.NewWriter(pw)
+
+	go func() {
+		defer pw.Close()
+
+		err := form.WriteField("name", "xd")
+		if err != nil {
+			app.Send(target, fmt.Sprintf("Something went wrong FeelsBadMan: %q", err))
+			return
+		}
+
+		file, err := os.Open(path) // path to image file
+		if err != nil {
+			app.Send(target, fmt.Sprintf("Something went wrong FeelsBadMan: %q", err))
+			return
+		}
+
+		w, err := form.CreateFormFile("file", path)
+		if err != nil {
+			app.Send(target, fmt.Sprintf("Something went wrong FeelsBadMan: %q", err))
+			return
+		}
+
+		_, err = io.Copy(w, file)
+		if err != nil {
+			app.Send(target, fmt.Sprintf("Something went wrong FeelsBadMan: %q", err))
+			return
+		}
+
+		form.Close()
+	}()
+
+	req, err := http.NewRequest(http.MethodPost, URL, pr)
+	if err != nil {
+		app.Send(target, fmt.Sprintf("Something went wrong FeelsBadMan: %q", err))
+		return
+	}
+	req.Header.Set("Content-Type", form.FormDataContentType())
+
+	httpClient := http.Client{}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		app.Send(target, fmt.Sprintf("Something went wrong FeelsBadMan: %q", err))
+		app.Log.Errorln("Error while sending HTTP request:", err)
+
+		return
+	}
+	defer resp.Body.Close()
+	app.Send(target, "Uploaded .. PogChamp")
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		app.Send(target, fmt.Sprintf("Something went wrong FeelsBadMan: %q", err))
+		app.Log.Errorln("Error while reading response:", err)
+		return
+	}
+
+	var reply = string(body[:])
+
+	app.Send(target, fmt.Sprintf("Removing file: %s", path))
+	os.Remove(path)
+	app.Send(target, reply)
 }
